88e9958ed1c2ea09565144b200d97c9690a71cb5
yereka <yereka@chigisoft.com>
2022-03-11 01:57:34 +0100

    initial c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * Entry point into the program 
+ * 
+ * Prints whats in put and return 0 
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
